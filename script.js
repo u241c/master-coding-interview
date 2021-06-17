577010b32b3e2edf1978e3a61618c208b090f657
u241c <jr4701.fsj@gmail.com>
2021-06-17 15:44:07 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+1.1// Big O and Scalability Beginnings how long an alogo takes to run
+// const nemo = ['nemo'];
+// // creating an array (nemo) 100xs
+// const large = new Array(100).fill('nemo')
+// function findNemo(array) {
+//     let t0 = performance.now();
+//     for (let i = 0; i < array.length; i++){
+//         if (array[i] === 'nemo'){
+//             console.log('found Nemo!');
+//         }
+//     }
+//     let t1 = performance.now();
+//     console.log('call to find nemo took' + (t1-t0) + 'milliseconds');
+// }
+// findNemo(large);
+
+1.2// const nemo = ['nemo'];
+// const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+// // creating an array (nemo) 100xs
+// const large = new Array(100).fill('nemo')
+// function findNemo(array) {
+    
+//     for (let i = 0; i < array.length; i++){
+//         if (array[i] === 'nemo'){
+//             console.log('found Nemo!');
+//         }
+//     }
+    
+    
+// }
+// findNemo(large); // 0(n) --> linear time to find nemo --> (n)can be replaced with anything means the Big 0 depend on the number of imputs(1)for nemo array (10)for the everyone array
+
+
+1.3 // in terms of Scalability O(1) - constant time - flat line
+const boxes = [0,1,2,3,4,5];
+function logFirstTwoBoxes(boxes){
+    console.log(boxes[0]); //O(1) # of operations
+    console.log(boxes[1]); //O(1) # of operations
+}
+logFirstTwoBoxes(boxes) // O(2) 2 operations
+
+// Big O excersize
+// function funChallenge(input) {
+//     let a = 10;//O(1)
+//     a = 50 + 3;//O(1)
+
+//     for (let i = 0; i < input.length; i++) { //O(n)
+//         anotherFunction(); //O(n)
+//         let stranger = true; //O(n)
+//         a++; //O(n)
+//     }
+//     return a; //O(1)
+// }
+// 3 + 4n
+
+// Big O excersize 2
+// function anotherFunChallenge(input) {
+//     let a = 5; //O(1)x's
+//     let b = 10; //O(1)
+//     let c = 50; //O(1)
+//     for (let i = 0; i < input; i++) { //O(n)
+//       let x = i + 1; //O(n)
+//       let y = i + 2; //O(n)
+//       let z = i + 3; //O(n)
+  
+//     }
+//     for (let j = 0; j < input; j++) { //O(n)
+//       let p = j * 2; //O(n)
+//       let q = j * 2; //O(n)
+//     }
+//     let whoAmI = "I don't know"; //O(1)
+//   }
+
+  // 4 + 7n
