15880d3911fb716278735a94f3dd2f03fd773b72
u241c <jr4701.fsj@gmail.com>
2021-06-23 17:05:32 -0500

    Add

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ function findNemo(array) {
 // Find 1st, Find Nth...
 const array = ['hi', 'my', 'teddy'];
 array[0];// O(1)
-array[array.length-1]// O(1)
+array[array.length-1]// O(1);
     
     
 
